8ef402625a4fe3ead8b7c5b5df884fec2ef4226a
marissa-mayes <marissa.mayes1289@gmail.com>
2020-10-11 21:09:47 -0500

    selection code completed, manager function works printing name input

--- Develop/app.js
+++ Develop/app.js
@@ -32,99 +32,176 @@ function start (){
             case "build team":
                 return needAnswers()
                 break;
-        }},       
+        }     
         
         
 
-function again() {
-    inquirer.prompt({
-        name: "again",
-        type: "confirm",
-        message: " want to do something else?",
-    }).then(answers =>{
-        switch (answers.again){
-            case true:
-                start()
-                break;
-                case false:
-                break;
+// function again() {
+//     inquirer.prompt({
+//         name: "again",
+//         type: "confirm",
+//         message: " want to do something else?",
+//     }).then(answers =>{
+//         switch (answers.again){
+//             case true:
+//                 start()
+//                 break;
+//                 case false:
+//                 break;
         
-    }},
+//     }},
 function needAnswers() {
     console.log("start")
-    return inquirer.prompt([
-        {
-            type: "list",
-            name: "teamMember",
+    return inquirer.prompt({
+            name: "action",
+            type:"list",
             message: "Who do you want to add to the team?",
             choices: [
                 "Manager",
                 "Engineer",
-                "Intern",
-            ]
-        },
-    ]).then(answers => {
-        switch (answers.action){
-            case " Add Manager":
+                "Intern"]
+            
+        }).then(answers => {
+        console.log ("answers")
+        switch (answers.action) {
+            case "Manager":
                 return manager()
                 break;
-                
-                case "Add Engineer":
+                console.log(answers.action,"where am i")
+                console.log(manager ,"function running?")
+                case "Engineer":
                     return engineer()
                     break;
                     
-                        case "Add Intern":
+                        case "Intern":
                             return intern()
                             break;
-        }},                  
-     function again(){
-         inquirer.prompt({
-             name: "again",
-             type: "confirm",
-             message: "want to do something else",
+        }
+        function manager() {
+            return inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "Please enter manager's name",
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "Please enter manager's email",
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "Please enter manager ID",
+                },
+                 //The following question is unique to managers only
+                {
+                    type: "input",
+                     name: "officeNumber",
+                     message: "Please enter manager's office room number",
+                 },
+             ])
+                 // Creates a new manager based on the input
+                 .then(answers => {
+                     console.log(answers, "made it")
+                     const manager = new Manager(answers.name, answers.id, 
+                        answers.email, answers.officeNumber)
+                        //made through push process.
+                     Team.push(manager)
+
+                })}
+                return needAnswers()
+            })}})
+        }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+    //  function again(){
+    //      inquirer.prompt({
+    //          name: "again",
+    //          type: "confirm",
+    //          message: "want to do something else",
             
-         }).then(answer =>{
-             switch (answer.again){
-                 case true:
-                     start ()
-                     break;
+    //      }).then(answer =>{
+    //          switch (answer.again){
+    //              case true:
+    //                  start ()
+    //                  break;
                      
-             }
-         })
-     },
+    //          }
+    //      })
+    //  },
 
 
-    function manager() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Please enter manager's name",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter manager's email",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please enter manager ID",
-        },
-        //The following question is unique to managers only
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Please enter manager's office room number",
-        },
-    ])
-        // Creates a new manager based on the input
-        .then(answers => {
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
-            newTeam.push(manager)
-            return needAnswers()
-        })});
-})})}
+    // function manager() {
+    // return inquirer.prompt([
+    //     {
+    //         type: "input",
+    //         name: "name",
+    //         message: "Please enter manager's name",
+    //     },
+    //     {
+    //         type: "input",
+    //         name: "email",
+    //         message: "Please enter manager's email",
+    //     },
+    //     {
+    //         type: "input",
+    //         name: "id",
+    //         message: "Please enter manager ID",
+    //     },
+    //     //The following question is unique to managers only
+    //     {
+    //         type: "input",
+    //         name: "officeNumber",
+    //         message: "Please enter manager's office room number",
+    //     },
+    // ])
+    //     // Creates a new manager based on the input
+    //     .then(answers => {
+    //         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+    //         newTeam.push(manager)
+    //         return needAnswers()
+    //     })});
 
     
 
